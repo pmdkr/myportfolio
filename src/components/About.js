22a9fc2d4a056f77fbcce9d535ed51c90d68bf0b
pramodlohra <pramod.lohra@nathcorp.com>
2024-12-16 20:31:48 +0530

    about page is completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 const About=()=>{
     return(
-        <div>
-            <h1>about page</h1>
+        <div className="flex justify-center align-middle">
+            <div className="flex justify-center w-3/4">
+                <p className="text-4xl">I am Pramod software developer, I am part of engineering services team of NathCorp Pvt Ltd.</p>
+            </div>
         </div>
     )
 }
